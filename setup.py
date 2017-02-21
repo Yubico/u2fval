e9f34e07b16b8444abc5b53e4049736b69567420
Dain Nilsson <dain@yubico.com>
2017-02-21 14:58:23 +0100

    Drop support for Python 2.6

--- setup.py
+++ setup.py
@@ -85,7 +85,6 @@ setup(
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
