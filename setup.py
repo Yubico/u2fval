0898f613ca8840801544360e938b386519a55e1b
Dain Nilsson <dain@yubico.com>
2017-02-21 14:41:18 +0100

    Explicitly depend on python-u2flib-server <5.

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setup(
         'console_scripts': ['u2fval=u2fval.cli:main']
     },
     data_files=data_files,
-    install_requires=['python-u2flib-server>=4.0', 'SQLAlchemy',
+    install_requires=['python-u2flib-server >= 4, <5', 'SQLAlchemy',
                       'WebOb', 'cachetools'],
     test_suite='test',
     tests_require=['WebTest'],
