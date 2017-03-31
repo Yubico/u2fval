33c4cd136a7c3d9aa9262f4721a3bf7ff17328a8
Dain Nilsson <dain@yubico.com>
2017-03-31 11:26:19 +0200

    Don't load config file in recalc script.

--- recalc-fingerprints.py
+++ recalc-fingerprints.py
@@ -1,5 +1,7 @@
 from __future__ import print_function
 
+import os
+os.environ['U2FVAL_SETTINGS'] = '/dev/null'  # Avoid loading config file
 from u2fval import app
 from u2fval.model import db, Certificate, _calculate_fingerprint
 from cryptography import x509
@@ -9,15 +11,16 @@ import click
 
 @click.command()
 @click.argument('db-uri')
-def migrate(db_uri):
+def rewrite_certs(db_uri):
     """Re-caclulates fingerprints for all certificates"""
     click.confirm('Re-calculate certificate fingerprints?', abort=True)
-
 
     app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 
     changed = 0
+    total = 0
     for cert in Certificate.query.all():
+        total += 1
         c = x509.load_der_x509_certificate(cert.der, default_backend())
         old_fp = cert.fingerprint
         new_fp = _calculate_fingerprint(c)
@@ -25,8 +28,8 @@ def migrate(db_uri):
             changed += 1
             cert.fingerprint = new_fp
     db.session.commit()
-    click.echo('Success! Updated %d certificates.' % changed)
+    click.echo('Success! %d/%d certificates were modified.' % (changed, total))
 
 
 if __name__ == '__main__':
-    migrate()
+    rewrite_certs()
